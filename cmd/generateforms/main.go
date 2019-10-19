95fb1c86c1d3cc11315f84aaba2e2f4304e0f451
nkall <nkallhovde@gmail.com>
2019-10-19 13:24:52 -0700

    Generation draft

---
+++ cmd/generateforms/main.go
@@ -0,0 +1,262 @@
+package generateforms
+
+import (
+	"bufio"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"text/template"
+	"time"
+
+	"github.com/nkall/compactnumber/compact"
+)
+
+type generationParams struct {
+	CompactFormsByLanguage map[string]compact.CompactForms
+	Timestamp              time.Time
+	CLDRVersion            string
+}
+
+type FileExtractResult struct {
+	Language     string
+	CLDRVersion  string
+	CompactForms compact.CompactForms
+}
+
+func sanitize(input string) string {
+	var result string
+	for _, char := range input {
+		switch {
+		case char >= 'a' && char <= 'z', char >= 'A' && char <= 'Z':
+			result += string(char)
+		}
+	}
+	return result
+}
+
+func extractFromFile(b []byte) (FileExtractResult, error) {
+	var file fileJson
+	err := json.Unmarshal(b, &file)
+	if err != nil {
+		return FileExtractResult{}, err
+	}
+
+	// Each file has just one language body, but the key isn't consistent
+	if len(file.Main) > 1 {
+		return FileExtractResult{}, errors.New("got multiple language bodies")
+	}
+
+	var body bodyJson
+	for _, fileBody := range file.Main {
+		body = fileBody
+		break
+	}
+
+	cldrVersion := body.Identity.Version.CLDRVersion
+	language := body.Identity.Language
+	if body.Identity.Territory != "" {
+		language = fmt.Sprintf("%s-%s", language, body.Identity.Territory)
+	}
+
+	defaultNumberingSystemBytes, ok := body.Numbers["defaultNumberingSystem"]
+	if !ok {
+		return FileExtractResult{}, errors.New("missing default numbering system")
+	}
+
+	var defaultNumberingSystem string
+	err = json.Unmarshal(defaultNumberingSystemBytes, &defaultNumberingSystem)
+	if err != nil {
+		return FileExtractResult{}, err
+	}
+
+	decimalFormatsBytes, ok := body.Numbers[fmt.Sprintf("decimalFormats-numberSystem-%s", defaultNumberingSystem)]
+	if !ok {
+		return FileExtractResult{}, errors.New(fmt.Sprintf("missing decimal formats for default numbering system %s", defaultNumberingSystem))
+	}
+
+	var decimalFormats decimalFormatsJson
+	err = json.Unmarshal(decimalFormatsBytes, &decimalFormats)
+	if err != nil {
+		return FileExtractResult{}, err
+	}
+
+	forms, err := extractCompactForms(decimalFormats)
+	if err != nil {
+		return FileExtractResult{}, err
+	}
+
+	return FileExtractResult{
+		Language:     language,
+		CLDRVersion:  cldrVersion,
+		CompactForms: forms,
+	}, nil
+}
+
+func extractCompactForms(formats decimalFormatsJson) (compact.CompactForms, error) {
+	if len(formats.Long.DecimalFormat) == 0 {
+		return nil, errors.New("missing long formats")
+	} else if len(formats.Short.DecimalFormat) == 0 {
+		return nil, errors.New("missing short formats")
+	}
+
+	longForms, err := extractCompactFormRules(formats.Long.DecimalFormat)
+	if err != nil {
+		return nil, err
+	}
+
+	shortForms, err := extractCompactFormRules(formats.Short.DecimalFormat)
+	if err != nil {
+		return nil, err
+	}
+
+	return compact.CompactForms{
+		compact.Long:  longForms,
+		compact.Short: shortForms,
+	}, nil
+}
+
+func extractCompactFormRules(formatRules map[string]string) ([]compact.CompactFormRule, error) {
+	countString := "-count-"
+	rules := make([]compact.CompactFormRule, 0, len(formatRules))
+
+	currType := -1
+	var currRule *compact.CompactFormRule
+	for formatName, formatPattern := range formatRules {
+		countIndex := strings.Index(formatName, countString)
+		if countIndex == -1 {
+			return nil, errors.New(fmt.Sprintf("missing count from %s", formatName))
+		}
+		typeStr := formatName[:countIndex]
+		typeNum, err := strconv.Atoi(typeStr)
+		if err != nil {
+			return nil, err
+		}
+
+		zeroesCount := strings.Count(formatPattern, "0")
+		if zeroesCount == 0 {
+			return nil, errors.New(fmt.Sprintf("missing zeroes from pattern: %s", formatPattern))
+		}
+
+		if typeNum != currType {
+			if currRule != nil {
+				rules = append(rules, *currRule)
+			}
+			currType = typeNum
+			currRule = &compact.CompactFormRule{
+				Type:                 typeNum,
+				ZeroesInPattern:      zeroesCount,
+				PatternsByPluralForm: make(map[string]string),
+			}
+		}
+
+		afterCountIndex := countIndex + len(countString) + 1
+		pluralForm := formatName[afterCountIndex:]
+
+		if currRule != nil {
+			currRule.PatternsByPluralForm[pluralForm] = formatPattern
+		} else {
+			return nil, errors.New("current rule is nil (bad programmer)")
+		}
+	}
+
+	return rules, nil
+}
+
+func writeFormsFile(params generationParams) error {
+	templateFile, err := ioutil.ReadFile("./forms.tmpl")
+	if err != nil {
+		return errors.New(fmt.Sprintf("error reading template file: %s", err.Error()))
+	}
+
+	templ, err := template.New("").Parse(string(templateFile))
+	if err != nil {
+		return errors.New(fmt.Sprintf("error parsing template file: %s", err.Error()))
+	}
+
+	f, err := os.Create("../../compact/forms.gen.go")
+	if err != nil {
+		return errors.New(fmt.Sprintf("error parsing template file: %s", err.Error()))
+	}
+
+	defer f.Close()
+
+	w := bufio.NewWriter(f)
+	err = templ.Execute(w, params)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	cldrPath := "../../cldr"
+	dirs, err := ioutil.ReadDir(cldrPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var genParams generationParams
+	genParams.CompactFormsByLanguage = make(map[string]compact.CompactForms)
+
+	for _, d := range dirs {
+		if !d.IsDir() {
+			log.Printf("skipping non-directory entry %s", d.Name())
+			continue
+		}
+
+		b, err := ioutil.ReadFile(fmt.Sprintf("%s/%s/numbers.json", cldrPath, d.Name()))
+		if err != nil {
+			log.Fatalf("error reading file at %s: %s", d.Name(), err.Error())
+		}
+
+		forms, err := extractFromFile(b)
+		if err != nil {
+			log.Fatalf("error extracting forms from %s: %s", d.Name(), err.Error())
+		}
+		genParams.CLDRVersion = forms.CLDRVersion
+		genParams.CompactFormsByLanguage[forms.Language] = forms.CompactForms
+	}
+
+	genParams.Timestamp = time.Now()
+
+	err = writeFormsFile(genParams)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Successfully generated compact formats file.")
+}
+
+// JSON unmarshalling structs
+type fileJson struct {
+	Main map[string]bodyJson `json:"main"`
+}
+
+type bodyJson struct {
+	Identity identityJson               `json:"identity"`
+	Numbers  map[string]json.RawMessage `json:"numbers"`
+}
+
+type identityJson struct {
+	Version struct {
+		CLDRVersion string `json:"_cldrVersion"`
+	} `json:"version"`
+	Language  string `json:"language"`
+	Territory string `json:"territory"`
+	Variant   string `json:"variant"`
+}
+
+type decimalFormatsJson struct {
+	Long  decimalFormatJson `json:"long"`
+	Short decimalFormatJson `json:"short"`
+}
+
+type decimalFormatJson struct {
+	DecimalFormat map[string]string `json:"decimalFormat"`
+}
