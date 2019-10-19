163c9359f95079eeeca7e297e8be9f1946a1e557
nkall <nkallhovde@gmail.com>
2019-10-19 11:52:21 -0700

    Make compact draft -- generation to come

---
+++ compact/models.go
@@ -0,0 +1,25 @@
+package compact
+
+// CompactType is an enum used to specify compaction settings.
+type CompactType string
+
+const (
+	// No compaction, e.g. 17,000,000.
+	None = CompactType("None")
+	// Long-form compaction, e.g. 17 million.
+	Long = CompactType("Long")
+	// Short-form compaction, e.g. 17M.
+	Short = CompactType("Short")
+)
+
+// CompactForms is a map of compaction type to a slice of form rules, ordered by rule type ascending.
+type CompactForms map[CompactType][]CompactFormRule
+
+// CompactFormRule is a single rule for compacting numbers greater than or equal to the value of "type".
+// ZeroesInPattern indicates the number of zero characters in the default "other" pattern, and PatternsByPluralForm
+// is a map of plural forms (e.g. one, few, many, other, etc.) to patterns.
+type CompactFormRule struct {
+	Type                 int
+	ZeroesInPattern      int
+	PatternsByPluralForm map[string]string
+}
